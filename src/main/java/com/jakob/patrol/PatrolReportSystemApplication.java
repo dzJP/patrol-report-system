0ef83fb52f0ffb036ab710bc2cd323420d0659e4
dzJP <Jakob_pietrzyk@outlook.com>
2026-03-22 15:15:47 +0100

    Initial commit

---
+++ src/main/java/com/jakob/patrol/PatrolReportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jakob.patrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrolReportSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrolReportSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jakob/patrol/PatrolReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakob.patrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrolReportSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
